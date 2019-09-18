ebdefc994939084204611c3dc06e58510dd54c8a
kgxl <z_jy0502@163.com>
2019-09-18 10:14:24 +0800

    1.升级gradle plugin 3.5 2.取消callback

--- app/src/main/java/com/qe/example/rxjava_retrofit_mvp/model/SearchModel.java
+++ app/src/main/java/com/qe/example/rxjava_retrofit_mvp/model/SearchModel.java
@@ -2,13 +2,12 @@ package com.qe.example.rxjava_retrofit_mvp.model;
 
 import com.qe.example.rxjava_retrofit_mvp.base.BaseApi;
 import com.qe.example.rxjava_retrofit_mvp.base.BaseModel;
-import com.qe.example.rxjava_retrofit_mvp.base.BaseObserver;
+import com.qe.example.rxjava_retrofit_mvp.base.RetrofitHelper;
 import com.qe.example.rxjava_retrofit_mvp.bean.Book;
-import com.qe.example.rxjava_retrofit_mvp.callback.Callback;
 
 import java.util.List;
 
-import io.reactivex.Observer;
+import io.reactivex.Observable;
 
 /**
  * Created by zjy on QE.
@@ -16,20 +15,7 @@ import io.reactivex.Observer;
 
 public class SearchModel implements BaseModel {
     //搜索书籍
-    public Observer<BaseApi<List<Book>>> getSearchData(final Callback<Book> callback) {
-        return new BaseObserver<List<Book>>() {
-            @Override
-            public void handleData(List<Book> book) {
-                for (Book b : book) {
-                    //接口回掉给P层
-                    callback.success(b);
-                }
-            }
-
-            @Override
-            public void errorMsg(String msg) {
-                callback.fail(msg);
-            }
-        };
+    public Observable<BaseApi<List<Book>>> getSearchData(String bookName) {
+        return RetrofitHelper.getBoobApi().searchBook(bookName);
     }
 }
--- app/src/main/java/com/qe/example/rxjava_retrofit_mvp/presenter/SearchPresenter.java
+++ app/src/main/java/com/qe/example/rxjava_retrofit_mvp/presenter/SearchPresenter.java
@@ -3,21 +3,14 @@ package com.qe.example.rxjava_retrofit_mvp.presenter;
 import android.text.TextUtils;
 
 import com.qe.example.rxjava_retrofit_mvp.base.BaseApi;
+import com.qe.example.rxjava_retrofit_mvp.base.BaseObserver;
 import com.qe.example.rxjava_retrofit_mvp.base.BasePresenter;
 import com.qe.example.rxjava_retrofit_mvp.base.BaseRx;
-import com.qe.example.rxjava_retrofit_mvp.base.RetrofitHelper;
 import com.qe.example.rxjava_retrofit_mvp.bean.Book;
-import com.qe.example.rxjava_retrofit_mvp.callback.Callback;
 import com.qe.example.rxjava_retrofit_mvp.model.SearchModel;
 import com.qe.example.rxjava_retrofit_mvp.view.SearchView;
 
 import java.util.List;
-
-import io.reactivex.Observer;
-import io.reactivex.Scheduler;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by zjy on QE.
@@ -27,15 +20,16 @@ public class SearchPresenter extends BasePresenter<SearchView, SearchModel> {
     //查找书籍
     public void search(String bookName) {
         if (!TextUtils.isEmpty(bookName)) {
-            RetrofitHelper.getBoobApi().searchBook(bookName).compose(BaseRx.<BaseApi<List<Book>>>io4main()).compose(mView.bindLifecycle()).subscribe(mModel.getSearchData(new Callback<Book>() {
+            mModel.getSearchData(bookName).compose(BaseRx.<BaseApi<List<Book>>>io4main()).compose(mView.bindLifecycle()).subscribe((new BaseObserver<Book>() {
+
                 @Override
-                public void success(Book book) {
+                public void handleData(Book book) {
                     //返回给V层
                     mView.searchSusses(book);
                 }
 
                 @Override
-                public void fail(String msg) {
+                public void errorMsg(String msg) {
                     mView.searchFail(msg);
                 }
             }));
